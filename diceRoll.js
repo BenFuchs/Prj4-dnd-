843a0225544b6e376d47ed5688bb4da98edfa713
BenFuchs <benayahcode@gmail.com>
2024-04-29 12:13:54 +0300

    added dice roll buttons

--- diceRoll.js
+++ diceRoll.js
@@ -1,34 +1,69 @@
+// dice roll functions 
 function rollD20() {
     var d20 = ''
     d20 = Math.floor(Math.random() * 21) + 1;
     document.getElementById("d20").innerHTML = d20
-    console.log(d20);    
+    console.log(d20);
 }
 
 function rollD12() {
     var d12 = ''
     d12 = Math.floor(Math.random() * 13) + 1;
     document.getElementById("d12").innerHTML = d12
-    console.log(d12);    
+    console.log(d12);
 }
 
 function rollD8() {
     var d8 = ''
     d8 = Math.floor(Math.random() * 9) + 1;
     document.getElementById("d8").innerHTML = d8
-    console.log(d8);    
+    console.log(d8);
 }
 
 function rollD6() {
     var d6 = ''
     d6 = Math.floor(Math.random() * 7) + 1;
     document.getElementById("d6").innerHTML = d6
-    console.log(d6);    
+    console.log(d6);
 }
 
 function rollD4() {
     var d4 = ''
     d4 = Math.floor(Math.random() * 5) + 1;
     document.getElementById("d4").innerHTML = d4
-    console.log(d4);    
+    console.log(d4);
+}
+// navbar stat functions 
+
+function addStat(inputId) {
+    var input = document.getElementById(inputId);
+    var currentValue = input.value.trim();
+    if (currentValue === "") {
+        input.value = "0";
+    } else {
+        var intValue = parseInt(currentValue);
+        if (!isNaN(intValue)) {
+            input.value = intValue + 1;
+        } else {
+            console.error("Invalid input. Please enter a valid number.");
+        }
+    }
+}
+
+function minusStat(inputId) {
+    var input = document.getElementById(inputId);
+    var currentValue = input.value.trim();
+
+
+    if (currentValue !== "" && !isNaN(currentValue)) {
+        // Parse the input value as an integer
+        var intValue = parseInt(currentValue);
+        if (intValue > 1) {
+            // Subtract 1 from the parsed value and update the input field
+            input.value = intValue - 1;
+        }
+    } else {
+        // If the input value is empty or not a valid number, handle it accordingly
+        console.error("Invalid input. Please enter a valid number.");
+    }
 }
