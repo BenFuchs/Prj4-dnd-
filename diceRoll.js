e1ba8289c0f5c6386dfaa6189e63a4520c34748b
BenFuchs <165273709+BenFuchs@users.noreply.github.com>
2024-05-16 20:01:52 +0300

    Update diceRoll.js

--- diceRoll.js
+++ diceRoll.js
@@ -4,35 +4,35 @@ const serverMods ='http://localhost:3000/Mod stats'
 // dice roll functions 
 function rollD20() {
     var d20 = ''
-    d20 = Math.floor(Math.random() * 21) + 1;
+    d20 = Math.floor(Math.random() * 20) + 1;
     document.getElementById("d20").innerHTML = d20
     console.log(d20);
 }
 
 function rollD12() {
     var d12 = ''
-    d12 = Math.floor(Math.random() * 13) + 1;
+    d12 = Math.floor(Math.random() * 12) + 1;
     document.getElementById("d12").innerHTML = d12
     console.log(d12);
 }
 
 function rollD8() {
     var d8 = ''
-    d8 = Math.floor(Math.random() * 9) + 1;
+    d8 = Math.floor(Math.random() * 8) + 1;
     document.getElementById("d8").innerHTML = d8
     console.log(d8);
 }
 
 function rollD6() {
     var d6 = ''
-    d6 = Math.floor(Math.random() * 7) + 1;
+    d6 = Math.floor(Math.random() * 6) + 1;
     document.getElementById("d6").innerHTML = d6
     console.log(d6);
 }
 
 function rollD4() {
     var d4 = ''
-    d4 = Math.floor(Math.random() * 5) + 1;
+    d4 = Math.floor(Math.random() * 4) + 1;
     document.getElementById("d4").innerHTML = d4
     console.log(d4);
 }
